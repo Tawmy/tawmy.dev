433ce4cbd25c6bfb14f2c11c0f03913f9fe29139
Tawmy <git@tawmy.dev>
2024-11-08 12:52:50 +0100

    Initial commit

---
+++ tawmy.dev/Program.cs
@@ -0,0 +1,21 @@
+using tawmy.dev.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
