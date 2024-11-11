4712f30bc51842f73489d06b67d9fa8c64112fe3
Tawmy <git@tawmy.dev>
2024-11-11 13:35:06 +0100

    Replace TailwindService with Tailwind.Extensions.AspNetCore

--- tawmy.dev/Program.cs
+++ tawmy.dev/Program.cs
@@ -1,21 +1,19 @@
+using Tailwind;
 using tawmy.dev.Components;
-using tawmy.dev.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
 
-if (builder.Environment.IsDevelopment() && builder.Configuration["DOTNET_RUNNING_IN_CONTAINER"] != "true")
-{
-    // if environment is dev and not running in container, add tailwind service for hot reload
-    builder.Services.AddHostedService<TailwindService>();
-}
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    _ = app.RunTailwind("css:watch");
+}
+else
 {
     app.UseExceptionHandler("/Error", true);
 }
